8cbab3bbd76d7cc84dc2876c5f3dc5c4e3c47780
AuliaAndini <auliaandini532@gmail.com>
2022-04-22 15:05:03 +0800

    Perbaikan Template dan persiapan fiture user

--- application/controllers/admin/Menu.php
+++ application/controllers/admin/Menu.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Menu extends CI_Controller {
     public function index()
     {
+        $this->load->view('template/manu');
         $data = array(
             'content'=> 'dashboard/index.php'
         );
